e1ee0249bb3f0b8db4c23a170afd4a3a2e0e1d70
Daniel Williams <danielpaulwill@gmail.com>
2022-08-20 11:35:58 -0400

    updated models

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,5 @@
 class Character < ApplicationRecord
+  has_many :items
+  has_many :attributes
+  belongs_to :user
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+  belongs_to :Location
+  has_many :options
 end
